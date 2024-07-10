ec482a1102b4e9590d1f63ed530560281b0d9ed1
istalia7 <alexis.perrotin02@gmail.com>
2024-07-10 13:39:21 +0200

    dropdown recap working both ways now

--- assets/recap.js
+++ assets/recap.js
@@ -1,12 +1,34 @@
 let plus = document.querySelector(".plus-sign");
 let line = document.querySelector("#line-opacity");
-let recap = document.querySelector(".recap_table1");
 let dropdownmenu1 = document.querySelector(".dropdownmenu1");
 let dropdownmenu2 = document.querySelector(".dropdownmenu2");
 let dropdownmenu3 = document.querySelector(".dropdownmenu3");
 plus.addEventListener("click", () => {
   line.classList.toggle("line_disappear");
-  dropdownmenu1.classList.toggle("dropdownmenu1_animated");
-  dropdownmenu2.classList.toggle("dropdownmenu2_animated");
-  dropdownmenu3.classList.toggle("dropdownmenu3_animated");
+
+  let allMenusShown =
+    dropdownmenu1.classList.contains("show") &&
+    dropdownmenu2.classList.contains("show") &&
+    dropdownmenu3.classList.contains("show");
+
+  if (allMenusShown) {
+    dropdownmenu1.classList.add("hide");
+    dropdownmenu2.classList.add("hide");
+    dropdownmenu3.classList.add("hide");
+    dropdownmenu1.style.opacity = 1;
+    dropdownmenu2.style.opacity = 1;
+    dropdownmenu3.style.opacity = 1;
+    setTimeout(function () {
+      dropdownmenu1.classList.remove("show", "hide");
+      dropdownmenu2.classList.remove("show", "hide");
+      dropdownmenu3.classList.remove("show", "hide");
+      dropdownmenu1.style.opacity = 0;
+      dropdownmenu2.style.opacity = 0;
+      dropdownmenu3.style.opacity = 0;
+    }, 600); // Delay matches animation duration
+  } else {
+    dropdownmenu1.classList.add("show");
+    dropdownmenu2.classList.add("show");
+    dropdownmenu3.classList.add("show");
+  }
 });
